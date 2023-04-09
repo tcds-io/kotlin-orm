25b2d7e67301b60b9a1b21b4741c0050820a953e
Thiago Cordeiro <thiagoguetten@gmail.com>
2023-04-09 19:25:20 -0300

    Added Unit transaction return
    Transactions might return something, but usually it will return nothing, before these changes, it was mandatory to inform the return type of void transactions and it added a default one

--- src/main/kotlin/io/tcds/orm/connection/Connection.kt
+++ src/main/kotlin/io/tcds/orm/connection/Connection.kt
@@ -8,6 +8,7 @@ interface Connection {
     suspend fun commit(): Boolean
     suspend fun rollback(): Boolean
     suspend fun <T> transaction(block: suspend () -> T)
+    suspend fun transaction(block: suspend () -> Unit) = transaction<Unit>(block)
     suspend fun read(sql: String, params: List<Param<*, *>> = emptyList()): Sequence<OrmResultSet>
     suspend fun write(sql: String, params: List<Param<*, *>> = emptyList()): Boolean
 }
