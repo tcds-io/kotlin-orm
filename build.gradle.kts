f11e829232fe8473d443c018ecaa5aea6dbfcef2
Thiago Cordeiro <thiagoguetten@gmail.com>
2023-05-28 11:26:19 -0300

    Publish to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,33 @@ val kotlinLoggingVersion: String by project
 val jacksonVersion: String by project
 val coroutinesVersion: String by project
 
-val buildVersion = System.getenv("VERSION") ?: "dev"
+object Publication {
+    const val group = "io.tcds"
+    val buildVersion: String = System.getenv("VERSION") ?: "dev"
+
+    object Sonatype {
+        val user: String = System.getenv("OSS_USER") ?: ""
+        val password: String = System.getenv("OSS_PASSWORD") ?: ""
+    }
+
+    object Github {
+        val user: String = System.getenv("GPR_USERNAME") ?: ""
+        val token: String = System.getenv("GPR_TOKEN") ?: ""
+    }
+
+    object Gpg {
+        val signingKeyId: String = System.getenv("GPG_KEY_ID") ?: ""
+        val signingKey: String = System.getenv("GPG_KEY") ?: ""
+        val signingPassword: String = System.getenv("GPG_KEY_PASSWORD") ?: ""
+    }
+}
 
 plugins {
     application
     `kotlin-dsl`
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 repositories {
@@ -45,6 +66,7 @@ tasks.withType<KotlinCompile>().configureEach {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
+
 tasks.test {
     useJUnitPlatform()
     testLogging {
@@ -57,18 +79,102 @@ tasks.test {
     }
 }
 
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+val javadocJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc)
+}
+
 publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            group = "io.tcds"
-            version = buildVersion
+            group = Publication.group
+            version = Publication.buildVersion
             url = uri("https://maven.pkg.github.com/tcds-io/kotlin-orm")
 
             credentials {
-                username = System.getenv("GPR_USERNAME")
-                password = System.getenv("GPR_TOKEN")
+                username = Publication.Github.user
+                password = Publication.Github.token
             }
+        }
+
+        maven {
+            name = "SonaType"
+            group = Publication.group
+            version = Publication.buildVersion
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = Publication.Sonatype.user
+                password = Publication.Sonatype.password
+            }
+        }
+    }
+
+    /**
+     * references:
+     * - https://docs.gradle.org/current/userguide/publishing_maven.html
+     * - https://github.com/LukasForst/ktor-plugins/blob/master/build.gradle.kts
+     * - https://devanshuchandra.medium.com/maven-central-publishing-using-gradle-and-gpg-signing-47b216179dd6
+     */
+    publications {
+        listOf("defaultMavenJava", "pluginMaven").forEach { publication ->
+
+            create<MavenPublication>(publication) {
+                // from(components["java"])
+                artifact(sourcesJar)
+                artifact(javadocJar)
+
+                pom {
+                    name.set("Kotlin Simple ORM")
+                    description.set("Kotlin Simple ORM is a simple but powerful database orm for kotlin applications")
+                    url.set("https://github.com/tcds-io/kotlin-orm")
+                    packaging = "jar"
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/tcds-io/kotlin-orm/blob/main/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("tcds-io")
+                            name.set("Thiago Cordeiro")
+                            email.set("thiago@tcds.io")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com:tcds-io/kotlin-orm.git")
+                        url.set("https://github.com/tcds-io/kotlin-orm")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKeyId = Publication.Gpg.signingKeyId
+    val signingKey = Publication.Gpg.signingKey
+    val signingPassword = Publication.Gpg.signingPassword
+
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications["pluginMaven"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(Publication.Sonatype.user)
+            password.set(Publication.Sonatype.password)
         }
     }
 }
