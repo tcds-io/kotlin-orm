1eb9e5439ffbdf8423dc5e18c414d1b552400e1b
Thiago Cordeiro <thiagoguetten@gmail.com>
2023-06-01 17:25:14 -0300

    Fixed nullable methods in the MapResultSet

--- src/main/kotlin/io/tcds/orm/MapOrmResultSet.kt
+++ src/main/kotlin/io/tcds/orm/MapOrmResultSet.kt
@@ -17,11 +17,11 @@ class MapOrmResultSet(private val data: OrmResult<*>) : OrmResultSet {
     override fun get(column: Column<*, LocalDate>): LocalDate = data[column.name]!! as LocalDate
     override fun get(column: Column<*, LocalDateTime>): LocalDateTime = data[column.name]!! as LocalDateTime
 
-    override fun nullable(column: Column<*, String?>): String? = data[column.name]!! as String?
-    override fun nullable(column: Column<*, Int?>): Int? = data[column.name]!! as Int?
-    override fun nullable(column: Column<*, Long?>): Long? = data[column.name]!! as Long?
-    override fun nullable(column: Column<*, Float?>): Float? = data[column.name]!! as Float?
-    override fun nullable(column: Column<*, Double?>): Double? = data[column.name]!! as Double?
-    override fun nullable(column: Column<*, LocalDate?>): LocalDate? = data[column.name]!! as LocalDate?
-    override fun nullable(column: Column<*, LocalDateTime?>): LocalDateTime? = data[column.name]!! as LocalDateTime?
+    override fun nullable(column: Column<*, String?>): String? = data[column.name] as String?
+    override fun nullable(column: Column<*, Int?>): Int? = data[column.name] as Int?
+    override fun nullable(column: Column<*, Long?>): Long? = data[column.name] as Long?
+    override fun nullable(column: Column<*, Float?>): Float? = data[column.name] as Float?
+    override fun nullable(column: Column<*, Double?>): Double? = data[column.name] as Double?
+    override fun nullable(column: Column<*, LocalDate?>): LocalDate? = data[column.name] as LocalDate?
+    override fun nullable(column: Column<*, LocalDateTime?>): LocalDateTime? = data[column.name] as LocalDateTime?
 }
