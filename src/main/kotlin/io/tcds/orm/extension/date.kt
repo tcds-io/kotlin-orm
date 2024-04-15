28f20e13917da241aa6e5b86602ae9c64d0bf413
Thiago Cordeiro <thiagoguetten@gmail.com>
2024-04-15 08:18:51 +0200

    Improved Instant x multiple type of dates conversion

--- src/main/kotlin/io/tcds/orm/extension/date.kt
+++ src/main/kotlin/io/tcds/orm/extension/date.kt
@@ -1,16 +1,23 @@
 package io.tcds.orm.extension
 
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.*
+import java.util.Date
 import java.sql.ResultSet as JdbcResultSet
 
 private val utc = ZoneId.of("UTC")
 
 fun JdbcResultSet.getInstant(name: String): Instant = getTimestamp(name).toInstant()
 
+// LocalDateTime <> Instant
 fun Instant.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(this, utc)
-fun Instant.toLocalDate(): LocalDate = LocalDate.ofInstant(this, utc)
 fun LocalDateTime.toInstant(): Instant = atZone(utc).toInstant()
+
+// LocalDate <> Instant
 fun LocalDate.toInstant(): Instant = atStartOfDay().atZone(utc).toInstant()
+fun Instant.toLocalDate(): LocalDate = LocalDate.ofInstant(this, utc)
+
+// Date <> Instant
+fun Instant.toDate(): Date = Date.from(this)
+
+// ZonedDateTime <> Instant
+fun Instant.toZonedDateTime(): ZonedDateTime = ZonedDateTime.ofInstant(this, utc)
