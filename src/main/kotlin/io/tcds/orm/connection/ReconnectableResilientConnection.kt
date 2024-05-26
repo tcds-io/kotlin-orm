c058f700682d3bfdf039492eecaf9b5181faf904
Thiago Cordeiro <thiagoguetten@gmail.com>
2024-05-26 19:32:29 +0200

    Improved reconnectable connection
    Added check to make sure connection is valid

--- src/main/kotlin/io/tcds/orm/connection/ReconnectableResilientConnection.kt
+++ src/main/kotlin/io/tcds/orm/connection/ReconnectableResilientConnection.kt
@@ -2,13 +2,19 @@ package io.tcds.orm.connection
 
 import java.sql.Connection
 
-open class ReconnectableResilientConnection(
+class ReconnectableResilientConnection(
     private val factory: () -> Connection,
 ) : ResilientConnection {
     private var connection: Connection? = null
 
     override fun instance(): Connection {
-        if (connection == null || connection!!.isClosed) connection = factory()
+        val requiresCreation = listOf(
+            connection == null,
+            connection?.isClosed,
+            connection?.isValid(5) == false,
+        ).any { it == true }
+
+        if (requiresCreation) connection = factory()
 
         return connection!!
     }
