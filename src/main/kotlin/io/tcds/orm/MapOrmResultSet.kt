3129650e0abdbc0e319ed0c384336e00d000f174
Thiago Cordeiro <thiagoguetten@gmail.com>
2024-09-09 17:03:16 +0200

    Fixed nullable instant from map resultset

--- src/main/kotlin/io/tcds/orm/MapOrmResultSet.kt
+++ src/main/kotlin/io/tcds/orm/MapOrmResultSet.kt
@@ -14,16 +14,18 @@ class MapOrmResultSet(private val data: OrmResult<*>) : OrmResultSet {
     override fun get(column: Column<*, Float>): Float = data[column.name]!! as Float
     override fun get(column: Column<*, Double>): Double = data[column.name]!! as Double
     override fun get(column: Column<*, Boolean>): Boolean = data[column.name]!! as Boolean
-    override fun get(column: Column<*, Instant>): Instant = when (val value = data[column.name]) {
-        is String -> Instant.parse(value)
-        is Instant -> value
-        else -> throw IllegalArgumentException("Unsupported type ${value?.javaClass}")
-    }
+    override fun get(column: Column<*, Instant>): Instant = instant(column.name)!!
 
     override fun nullable(column: Column<*, String?>): String? = data[column.name] as String?
     override fun nullable(column: Column<*, Int?>): Int? = data[column.name] as Int?
     override fun nullable(column: Column<*, Long?>): Long? = data[column.name] as Long?
     override fun nullable(column: Column<*, Float?>): Float? = data[column.name] as Float?
     override fun nullable(column: Column<*, Double?>): Double? = data[column.name] as Double?
-    override fun nullable(column: Column<*, Instant?>): Instant? = data[column.name] as Instant?
+    override fun nullable(column: Column<*, Instant?>): Instant? = instant(column.name)
+
+    private fun instant(name: String) = when (val value = data[name]) {
+        is String -> Instant.parse(value)
+        is Instant -> value
+        else -> null
+    }
 }
